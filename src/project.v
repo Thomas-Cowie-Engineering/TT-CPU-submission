92b8e7184ebbd262596935dd6976262e045cdbbb
Thomas Cowie <TCowie1@sheffield.ac.uk>
2026-07-17 12:39:59 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -7,7 +7,7 @@
 
 `default_nettype none
 
-module tt_um_LMC (
+module tt_um_ThomasCowieEngineering_LMC (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
